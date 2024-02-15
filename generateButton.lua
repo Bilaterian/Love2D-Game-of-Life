8b051bb875d05cdd284d4f2a194cd166fb748b2e
Bilaterian <lanceopaje@gmail.com>
2024-02-15 10:23:11 -0500

    added a new button and shrunk the board size
    button is clickable and generates a board

--- generateButton.lua
+++ generateButton.lua
@@ -1,13 +1,13 @@
 local generateButton = {}
 local seed = 0
 local button = {
-    width = 10,
-    heigt = 10,
-    text = "generate",
+    width = 400,
+    height = 100,
+    offsetX = 0,
+    offsetY = 0,
+    text = "Generate",
     clicked = false,
 }
-
-
 
 function generateButton.setSeed(newSeed)
     if newSeed == nil then
@@ -15,7 +15,23 @@ function generateButton.setSeed(newSeed)
     else
         seed = newSeed
     end
-    
+end
+
+function generateButton.checkBounds(x,y)
+    if x > button.offsetX and x < button.offsetX + button.width then
+        if y > button.offsetY and y < button.offsetY + button.height then
+            return true
+        end
+    end
+    return false
+end
+
+function generateButton.getButtonDimensions()
+    return {button.offsetX, button.offsetY, button.width, button.height}
+end
+
+function generateButton.getText()
+    return button.text
 end
 
 return generateButton
--- main.lua
+++ main.lua
@@ -10,9 +10,10 @@ local colors = {
 local PIXEL_SIZE = 5
 
 local width, height, flags = love.window.getMode()
-
-local boardWidth = math.floor(width/PIXEL_SIZE)
-local boardHeight = math.floor(height/PIXEL_SIZE)
+local offsetX = 400
+local offsetY = 0
+local boardWidth = math.floor((width - offsetX)/PIXEL_SIZE)
+local boardHeight = math.floor((height- offsetY)/PIXEL_SIZE)
 
 local boards = require "boards"
 local generateButton = require "generateButton"
@@ -24,6 +25,15 @@ function love.load()
     boards.populateBoard()
 end
 
+function love.mousepressed(x, y, button, istouch)
+    if button == 1 then
+        --all button checks will happen here
+        if generateButton.checkBounds(x, y) == true then
+            boards.populateBoard()
+        end
+    end
+end
+
 function love.update(dt)
 --want to check board here
     boards.transferBoards()
@@ -32,15 +42,29 @@ end
 function love.draw()
     local i = 1
     local j = 1
-    for x = 0, width - 1, PIXEL_SIZE do
-        for y = 0, height - 1, PIXEL_SIZE do
+    
+    --draws board
+    for x = 0, width - 1 - offsetX, PIXEL_SIZE do
+        for y = 0, height - 1 - offsetY, PIXEL_SIZE do
             if boards.getCellValue(i, j) == 1 then
                 love.graphics.setColor(colors[5])
-                love.graphics.rectangle("fill", x, y, PIXEL_SIZE, PIXEL_SIZE)
+                love.graphics.rectangle("fill", x + offsetX, y + offsetY, PIXEL_SIZE, PIXEL_SIZE)
             end
             j = j + 1
         end
         j = 1
         i = i + 1
     end
+    
+    --draws button
+    local dimensions = generateButton.getButtonDimensions()
+    love.graphics.setColor(colors[1])
+    love.graphics.rectangle("fill", dimensions[1], dimensions[2], dimensions[3], dimensions[4])
+    
+    local font = love.graphics.newFont(24)
+    local text = love.graphics.newText(font, generateButton.getText())
+    local width = (dimensions[3] - font:getWidth(generateButton.getText())) / 2
+    local height = (dimensions[4] - font:getHeight(generateButton.getText())) / 2
+    love.graphics.setColor(colors[4])
+    love.graphics.draw(text, width, height)
 end
